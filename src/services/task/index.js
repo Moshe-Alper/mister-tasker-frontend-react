a05cce1eed8386520916783195f3f35af772165c
Moshik Alper <moshikalper@gmail.com>
2025-03-14 14:17:06 +0200

    Init repo

---
+++ src/services/task/index.js
@@ -0,0 +1,31 @@
+const { DEV, VITE_LOCAL } = import.meta.env
+
+import { getRandomIntInclusive, makeId } from '../util.service'
+
+import { taskService as local } from './task.service.local'
+import { taskService as remote } from './task.service.remote'
+
+function getEmptyTask() {
+	return {
+		vendor: makeId(),
+		speed: getRandomIntInclusive(80, 240),
+		msgs: [],
+	}
+}
+
+function getDefaultFilter() {
+    return {
+        txt: '',
+        minSpeed: '',
+        sortField: '',
+        sortDir: '',
+    }
+}
+
+const service = VITE_LOCAL === 'true' ? local : remote
+export const taskService = { getEmptyTask, getDefaultFilter, ...service }
+
+// Easy access to this service from the dev tools console
+// when using script - dev / dev:local
+
+if (DEV) window.taskService = taskService
